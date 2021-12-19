d3d1778d04924caa0768e13272517e10750bd7a6
Pavel Shatalov <shatalovp@gmail.com>
2021-12-19 08:45:13 +0700

    Add new function

--- utils.go
+++ utils.go
@@ -1,8 +1,17 @@
 package utils
 
-func Contains(slice []string, str string) bool {
+func Contains(slice []string, x string) bool {
 	for _, v := range slice {
-		if v == str {
+		if v == x {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt(slice []int, x int) bool {
+	for _, v := range slice {
+		if v == x {
 			return true
 		}
 	}
